124b7e4843500af5025f8798097754df81b02ad3
Andrew Williams <andrew.williams@iris.co.uk>
2022-03-23 13:26:18 +0000

    fix(unit tests): the one test now works

--- CheckArtifactConsistencyTask/tests/inputs/Release_ReleaseIdNotProvided.ts
+++ CheckArtifactConsistencyTask/tests/inputs/Release_ReleaseIdNotProvided.ts
@@ -1,11 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import ma = require('azure-pipelines-task-lib/mock-answer');
 import tmrm = require('azure-pipelines-task-lib/mock-run');
+import { timeStamp } from 'console';
 import path = require('path');
 
 const taskPath = path.join(__dirname, '..', '..', 'src', 'CheckArtifactConsistencyTask.js');
 const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskPath);
 
-tmr.setInput('System.TeamFoundationCollectionUri', 'FakeUri');
+// see https://github.com/microsoft/azure-pipelines-task-lib/issues/593 for why we're setting variables like this
+process.env['SYSTEM_TEAMFOUNDATIONCOLLECTIONURI'] = 'FakeUri';
 
 tmr.run();
